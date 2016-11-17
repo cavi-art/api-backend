e54f06fbc220f53980e48d7a067adef4b437d39e
ssaavedra <ssaavedra@7ef80a5c-f9c9-4c3c-9a70-3e98a7bd2970>
2016-11-17 14:27:11 +0000

    Add initial models
    git-svn-id: https://dalila.sip.ucm.es/svn_caviart/asistente-validacion/api-backend@1444 7ef80a5c-f9c9-4c3c-9a70-3e98a7bd2970

--- caviart/models.py
+++ caviart/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from rest_framework.authentication import get_user_model
 
-# Create your models here.
+
+class Project(models.Model):
+    uuid = models.ShortUUIDField(primary_key=True)
+    owner = models.ForeignKey(get_user_model())
+
+class ProjectFile(models.Model):
+    path = models.FilePathField(max_length=255)
+    file_type = models.CharField(max_length=10)
+    last_mod = models.DateField(auto_now=True)
+    content = models.FileField()
+
+class VerificationFile(models.Model):
+    source = models.ForeignKey(ProjectFile)
+    last_mod = models.DateField(auto_now=True)
+    content = models.FileField()
+
+class ProofObligation(models.Model):
+    clir = models.ForeignKey(VerificationFile)
+    last_mod = models.DateField(auto_now=True)
+    goal = models.CharField(max_length=100)
+    strategies = models.TextField()
+    status = models.CharField(max_length=1)
+    
