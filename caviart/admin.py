d520ec84c9b3dd0bd1357795727910bd809d50d5
ssaavedra <ssaavedra@7ef80a5c-f9c9-4c3c-9a70-3e98a7bd2970>
2016-11-17 14:27:16 +0000

    Register our models with the Django Admin interface
    git-svn-id: https://dalila.sip.ucm.es/svn_caviart/asistente-validacion/api-backend@1451 7ef80a5c-f9c9-4c3c-9a70-3e98a7bd2970

--- caviart/admin.py
+++ caviart/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from caviart.models import (Project, ProjectFile, ProofObligation,
+    VerificationFile)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProjectFile)
+class ProjectFileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProofObligation)
+class VerificationFileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(VerificationFile)
+class ProofObligationAdmin(admin.ModelAdmin):
+    pass
+
