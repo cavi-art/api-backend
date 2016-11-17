40b00cc550e7a2004600f1309b2413a316c563b4
ssaavedra <ssaavedra@7ef80a5c-f9c9-4c3c-9a70-3e98a7bd2970>
2016-11-17 14:27:22 +0000

    Configure file uploads while in development mode
    git-svn-id: https://dalila.sip.ucm.es/svn_caviart/asistente-validacion/api-backend@1460 7ef80a5c-f9c9-4c3c-9a70-3e98a7bd2970

--- api_config/urls.py
+++ api_config/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from caviart.urls import urlpatterns as caviart_urls
@@ -22,4 +24,4 @@ from caviart.urls import urlpatterns as caviart_urls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(caviart_urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
