90a483d7b8d2afe69093f8cc379f666cda7fb1d5
ssaavedra <ssaavedra@7ef80a5c-f9c9-4c3c-9a70-3e98a7bd2970>
2016-11-17 14:27:27 +0000

    Add Celery for the task queue
    git-svn-id: https://dalila.sip.ucm.es/svn_caviart/asistente-validacion/api-backend@1467 7ef80a5c-f9c9-4c3c-9a70-3e98a7bd2970

---
+++ api_config/celery.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_config.settings')
+
+app = Celery(
+    'api_config',
+    broker='amqp://',
+    # backend='amqp://',
+    # result_expires=300,
+)
+
+# Using a string here means the worker doesn't have to serialize the
+# configuration object.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# load task modules from registered Django app configs.
+app.autodiscover_tasks()
+
+
+## Debug task...
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
